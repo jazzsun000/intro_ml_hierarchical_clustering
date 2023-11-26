55c56b5529383b629be4c702c6b83ac1eb50c2cf
jazzsun10 <psun10@usfca.edu>
2023-11-25 21:35:01 -0800

    add explanation guide

--- hierarchical_clustering_app.py
+++ hierarchical_clustering_app.py
@@ -1,6 +1,3 @@
-# Required installations:
-# pip install streamlit scikit-learn matplotlib scipy
-
 import streamlit as st
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage
@@ -29,14 +26,47 @@ def plot_dendrogram(Z, max_d=None):
 
 st.title("Hierarchical Clustering Demonstrator")
 
-# Sidebar settings
+st.write("""
+    Welcome to the Hierarchical Clustering Demonstrator. This app allows you to visualize 
+    hierarchical clustering using different linkage methods. Adjust the sliders and dropdown in the sidebar 
+    to change the parameters and observe how the dendrogram changes.
+""")
+
+# Sidebar settings with explanations
 st.sidebar.header("Hierarchical Clustering Settings")
+st.sidebar.markdown("""
+    **Number of Samples**  
+    Choose how many data points to generate for clustering.
+""")
 num_samples = st.sidebar.slider("Number of Samples", 10, 200, 100)
+
+st.sidebar.markdown("""
+    **Number of Features**  
+    Select the number of features (dimensions) for each data point.
+""")
 num_features = st.sidebar.slider("Number of Features", 2, 5, 2)
+
+st.sidebar.markdown("""
+    **Linkage Method**  
+    Select the method used to calculate the distance between clusters.
+""")
 linkage_method = st.sidebar.selectbox(
     "Linkage Method",
     ("ward", "complete", "average", "single")
 )
+
+st.sidebar.markdown("""
+    **Cut-off Distance**  
+    Set a threshold to define the maximum distance between clusters. Clusters above this 
+    distance will not be joined.
+""")
+max_d = st.sidebar.slider("Cut-off Distance", 0.0, 200.0, 25.0)
+
+st.markdown("""
+    **Data Generation**  
+    The dataset is generated with a specified number of samples and features, 
+    which are randomly distributed in space. 
+""")
 
 # Generate synthetic data
 X, _ = make_blobs(n_samples=num_samples, n_features=num_features, random_state=42)
@@ -44,15 +74,23 @@ X, _ = make_blobs(n_samples=num_samples, n_features=num_features, random_state=4
 # Perform hierarchical clustering
 Z = linkage(X, method=linkage_method)
 
-# Show dendrogram
+# Show dendrogram with explanations
 st.subheader(f"Dendrogram ({linkage_method} linkage)")
+st.write("""
+    A dendrogram visualizes the process of hierarchical clustering. The y-axis represents 
+    the distance between clusters being merged, while the x-axis represents the individual 
+    data points or merged clusters.
+""")
 plot_dendrogram(Z)
 
-# Allow user to set a cut-off distance to determine clusters
-max_d = st.sidebar.slider("Cut-off Distance", 0.0, 200.0, 25.0)
+# Show truncated dendrogram with explanations
 st.subheader(f"Truncated Dendrogram ({linkage_method} linkage) with Cut-off Distance")
+st.write("""
+    The truncated dendrogram shows the last few merges of the hierarchical clustering process. 
+    The horizontal line represents the cut-off distance used to define clusters.
+""")
 plot_dendrogram(Z, max_d=max_d)
 
-# Determine and report the number of clusters as per cut-off distance
+# Determine and report the number of clusters
 clusters = fcluster(Z, max_d, criterion='distance')
-st.write(f'Number of clusters formed: {len(set(clusters))}')
+st.write(f'Number of clusters formed at cut-off distance {max_d}: {len(set(clusters))}')
